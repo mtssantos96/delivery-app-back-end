8a077da3deb0e91171f9de941ac59cdaca61a163
mtssantos96 <matheus.p.1996@hotmail.com>
2023-07-18 20:18:12 -0300

    feat: configurando o backend

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -11,12 +11,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+    `${process.env.MYSQLDATABASE || 'delivery-app'}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
